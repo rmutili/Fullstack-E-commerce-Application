458d5c49c1abd261e34421779f94395ea66ebbc7
Raphael Mutili <kqraphael@yahoo.com>
2024-01-03 11:57:55 +0300

    Step 8 completed. Fetching data from our backend using React - done.

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,8 +1,18 @@
 import { Grid, Heading } from "@chakra-ui/react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import ProductCard from "../components/ProductCard";
-import products from "../products";
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get("/products");
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
+
   return (
     <>
       <Heading as="h2" mb="8" fontSize="xl">
@@ -11,7 +21,7 @@ const HomeScreen = () => {
 
       <Grid templateColumns="1fr 1fr 1fr 1fr" gap="8">
         {products.map((prod) => (
-          <ProductCard product={prod} />
+          <ProductCard key={prod.id} product={prod} />
         ))}
       </Grid>
     </>
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,11 +1,20 @@
 import { Button, Flex, Grid, Heading, Image, Text } from "@chakra-ui/react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { Link as RouterLink, useParams } from "react-router-dom";
 import Rating from "../components/Rating";
-import products from "../products";
 
 const ProductScreen = () => {
   const { id } = useParams();
-  const product = products.find((prod) => prod._id === id);
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/products/${id}`);
+      setProduct(data);
+    };
+    fetchProduct();
+  }, [id]);
 
   return (
     <>
