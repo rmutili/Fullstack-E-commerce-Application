d1541752acfd496b0f4841f0a44afd45b7ab59f2
Raphael Mutili <kqraphael@yahoo.com>
2024-01-03 13:12:13 +0300

    Step 14 completed. Improve console log messages done.

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import products from "./data/products.js"; // Importing products from data/products.js in Node you must add the .js extension
 import connectDB from "./config/db.js";
+import colors from "colors";
 
 dotenv.config(); // Allow to load environment variables from a .env file into process.env
 
@@ -25,5 +26,7 @@ app.get("/api/products/:id", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.red.bold
+  );
 });
