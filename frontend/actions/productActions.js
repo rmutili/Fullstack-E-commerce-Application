567a49524d4a55f5de227ef9c51aefe364e31b34
Raphael Mutili <kqraphael@yahoo.com>
2024-01-05 09:52:14 +0300

    Step 22 completed. Create our first reducer done.

--- frontend/actions/productActions.js
+++ frontend/actions/productActions.js
@@ -1,9 +1,10 @@
+import axios from "axios";
+
 import {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_FAIL,
 } from "../constants/productConstants";
-import axios from "axios";
 
 export const listProducts = () => async (dispatch) => {
   try {
