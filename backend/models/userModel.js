036573de31b7ecc5bf1717aa3e63ec5d99fb48ae
Raphael Mutili <kqraphael@yahoo.com>
2024-01-06 16:13:00 +0300

    Step 34 completed. User registration done.

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -32,6 +32,18 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
+userSchema.pre("save", async function (next) {
+  // console.log("password is being modified");
+  if (!this.isModified("password")) {
+    // console.log("password is not being modified");
+    next();
+  }
+
+  // console.log("password is being hashed");
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
 const User = mongoose.model("User", userSchema);
 
 export default User;
