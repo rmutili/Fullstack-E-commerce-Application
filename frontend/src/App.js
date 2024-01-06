953258fd474159730e42765168dbfd3ddf693a20
Raphael Mutili <kqraphael@yahoo.com>
2024-01-06 17:36:43 +0300

    Step 39 completed. User Register, Constants, Reducer and Screen done.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
 import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
 
 const App = () => {
   return (
@@ -25,6 +26,7 @@ const App = () => {
           <Route path="/cart/:id" element={<CartScreen />} />
           <Route path="/cart" element={<CartScreen />} />
           <Route path="/login" element={<LoginScreen />} />
+          <Route path="/register" element={<RegisterScreen />} />
         </Routes>
       </Flex>
       <Footer />
--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -4,15 +4,15 @@ import {
   USER_LOGIN_SUCCESS,
   USER_LOGIN_FAIL,
   USER_LOGOUT,
-  // USER_REGISTER_REQUEST,
-  // USER_REGISTER_SUCCESS,
-  // USER_REGISTER_FAIL,
-  USER_DETAILS_REQUEST,
-  USER_DETAILS_SUCCESS,
-  USER_DETAILS_FAIL,
-  USER_UPDATE_PROFILE_REQUEST,
-  USER_UPDATE_PROFILE_SUCCESS,
-  USER_UPDATE_PROFILE_FAIL,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
+  USER_REGISTER_FAIL,
+  // USER_DETAILS_REQUEST,
+  // USER_DETAILS_SUCCESS,
+  // USER_DETAILS_FAIL,
+  // USER_UPDATE_PROFILE_REQUEST,
+  // USER_UPDATE_PROFILE_SUCCESS,
+  // USER_UPDATE_PROFILE_FAIL,
 } from "../constants/userConstants";
 
 // getState allows us to get our entire state tree
@@ -50,3 +50,43 @@ export const logout = () => (dispatch) => {
   localStorage.removeItem("userInfo");
   dispatch({ type: USER_LOGOUT });
 };
+
+export const register = (name, email, password) => async (dispatch) => {
+  try {
+    dispatch({ type: USER_REGISTER_REQUEST });
+
+    // We want to send JSON data in the body, so we need to set the content type to application/json
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    // We want to send a POST request to /api/users, and we want to send the name, email, and password in the body
+    const { data } = await axios.post(
+      "/api/users",
+      { name, email, password },
+      config
+    );
+
+    // If we get a successful response, we want to dispatch USER_REGISTER_SUCCESS and set the payload to data
+    dispatch({ type: USER_REGISTER_SUCCESS, payload: data });
+
+    // We also want to dispatch USER_LOGIN_SUCCESS and set the payload to data so that the user is logged in immediately after registering
+    dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
+
+    // We also want to save the user info in local storage so that the user is still logged in after refreshing the page
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  } catch (error) {
+    // If we get an error, we want to dispatch USER_REGISTER_FAIL and set the payload to the error message
+    dispatch({
+      type: USER_REGISTER_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message, // error.response.data.message is the error message from the backend
+    });
+  }
+};
+
+// export const getUserDetails = (id) => async (dispatch, getState) => {
