d531b8bb0fe7c5672a2bf68589090edba6133e75
Raphael Mutili <kqraphael@yahoo.com>
2024-01-06 14:33:18 +0300

    Step 32 completed. Using json web Tokens(JWT) done.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import User from "../models/userModel.js";
 import expressAsyncHandler from "express-async-handler";
+import generateToken from "../utils/generateToken.js";
 
 const authUser = expressAsyncHandler(async (req, res) => {
   const { email, password } = req.body;
@@ -12,6 +13,7 @@ const authUser = expressAsyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
+      token: generateToken(user._id),
     });
   } else {
     res.status(401);
