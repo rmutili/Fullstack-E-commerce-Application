6319036f589ae7a95197a6f5e289c392d7a8c8eb
Raphael Mutili <kqraphael@yahoo.com>
2024-01-12 09:35:03 +0300

    Step 53 completed. Order pay reducer and action done.

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -5,10 +5,10 @@ import {
   ORDER_CREATE_FAIL,
   ORDER_DETAILS_REQUEST,
   ORDER_DETAILS_SUCCESS,
-  ORDER_DETAILS_FAIL
-  //   ORDER_PAY_REQUEST,
-  //   ORDER_PAY_SUCCESS,
-  //   ORDER_PAY_FAIL,
+  ORDER_DETAILS_FAIL,
+  ORDER_PAY_REQUEST,
+  ORDER_PAY_SUCCESS,
+  ORDER_PAY_FAIL
   //   ORDER_LIST_MY_REQUEST,
   //   ORDER_LIST_MY_SUCCESS,
   //   ORDER_LIST_MY_FAIL,
@@ -77,3 +77,37 @@ export const getOrderDetails = (id) => async (dispatch, getState) => {
     });
   }
 };
+
+const updateOrderToPaid =
+  (orderId, paymentResult) => async (dispatch, getState) => {
+    try {
+      dispatch({ type: ORDER_PAY_REQUEST });
+
+      const {
+        userLogin: { userInfo }
+      } = getState();
+
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${userInfo.token}`
+        }
+      };
+
+      const { data } = await axios.put(
+        `/api/orders/${orderId}/pay`,
+        paymentResult,
+        config
+      );
+      dispatch({ type: ORDER_PAY_SUCCESS, payload: data });
+    } catch (error) {
+      console.log(error);
+      dispatch({
+        type: ORDER_PAY_FAIL,
+        payload:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message
+      });
+    }
+  };
--- frontend/src/reducers/orderReducer.js
+++ frontend/src/reducers/orderReducer.js
@@ -4,7 +4,11 @@ import {
   ORDER_CREATE_FAIL,
   ORDER_DETAILS_REQUEST,
   ORDER_DETAILS_SUCCESS,
-  ORDER_DETAILS_FAIL
+  ORDER_DETAILS_FAIL,
+  ORDER_PAY_REQUEST,
+  ORDER_PAY_SUCCESS,
+  ORDER_PAY_FAIL,
+  ORDER_PAY_RESET
 } from "../constants/orderConstants";
 
 export const orderCreateReducer = (state = {}, action) => {
@@ -43,5 +47,28 @@ export const orderDetailsReducer = (state = { loading: true }, action) => {
       };
     default:
       return state;
+  }
+};
+
+export const orderPayReducer = (state = {}, action) => {
+  switch (action.type) {
+    case ORDER_PAY_REQUEST: // if the action type is ORDER_PAY_REQUEST
+      return {
+        loading: true // set loading to true
+      };
+    case ORDER_PAY_SUCCESS: // if the action type is ORDER_PAY_SUCCESS
+      return {
+        loading: false, // set loading to false
+        success: true // set success to true
+      };
+    case ORDER_PAY_FAIL: // if the action type is ORDER_PAY_FAIL
+      return {
+        loading: false, // set loading to false
+        error: action.payload // set error to the action payload
+      };
+    case ORDER_PAY_RESET:
+      return {};
+    default:
+      return state; // return the current state
   }
 };
